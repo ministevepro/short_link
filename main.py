d2f3b666a84ef00b5cd714f66a84045f68c7f293
ministevepro <ivankrovi27@gmail.com>
2025-09-18 22:59:56 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+from pprint import pprint
+import requests
+url= "https://api-ssl.bitly.com/v4/user"
+
+
+tokeen="fe9acb6db9a23074432d30ed34d4ccf57132506d"
+
+
+
+headers = {
+ "Authorization": f"Bearer {tokeen}",
+ "Content-Type": "application/json"
+}
+
+
+
+
+
+
+while True:
+    print("программа работ со ссылками бitly")
+    print("1) Получить инвормацию о профиле")
+    print("2) Сократить ссылку")
+    print("3) выход")
+    x=input("виберите функцию: ")
+
+    
+    if x=="1":
+        response = requests.get(url,headers=headers)    
+        response.raise_for_status() 
+        pprint(response.json())
+    if x=="2":
+        user_url=input("введите ссылку для сокращения: " )
+        url = "https://api-ssl.bitly.com/v4/shorten"
+        params={
+            "long_url" : user_url
+        }
+    
+
+        response = requests.post(url,json=params,headers=headers)
+        try:
+            response.raise_for_status()
+            pprint(response.json())
+        except:
+            print("напишите правильную ссылку")
+    if x=="3":
+        exit()
+
+
+
+
+
